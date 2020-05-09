2968389e71ac1986e4e0be8407e57d3d90dad5bd
ola357 <alliolaoluwa@gmail.com>
2020-05-09 08:36:54 +0100

    project setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Header from './components/Header'
 import app from './app';
-
+/*
 const startApp = async () => {
   const header = document.querySelector('[data-app-name]');
   if (!header) return;
@@ -9,3 +12,19 @@ const startApp = async () => {
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
+*/
+class HelloMessage extends React.Component {
+  render() {
+      return <div>
+          <Header/>
+          <div className="container">
+              <h1>Hello {this.props.name}</h1>
+          </div>
+      </div>
+  }
+}
+
+let App = document.querySelector('[data-app-name]');
+const programName = await app();
+
+ReactDOM.render(<HelloMessage name={programName} />, App);
